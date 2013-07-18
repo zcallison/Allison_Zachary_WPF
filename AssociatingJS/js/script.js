35de7266cc287b8d998231f6104a5c7bf33a2b8c
Zach Allison <zcallison@gmail.com>
2013-07-18 13:09:52 -0500

    Concatenating

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,6 +1,2 @@
-// Expressions  :  Expressions with Arrays
+// Expressions  :  Assignment Operators
 
-var orangeBins = [234, 567, 883];
-
-var total = orangeBins[0] + orangeBins[1] + orangeBins[2];
-console.log(total);
