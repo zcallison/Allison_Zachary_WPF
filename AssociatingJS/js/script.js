ed398e9ef191c23bcb8f59d19753d8fb5eb73405
Zach Allison <zcallison@gmail.com>
2013-07-18 12:45:38 -0500

    Using Modulo

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,9 +1,4 @@
-// Expressions  :  Arithmetic Operators
+// Expressions  :  Modulo Operator
 
-//area of the triangle is half of the width times the height
-var width = 8;
-var height = 7;
-
-var area = width * height/2;
-console.log(area);
-
+var remainder = 32 % 2;
+console.log(remainder);
