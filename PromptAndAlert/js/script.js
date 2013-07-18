8dd509867e2385792dbd32cd34773a53115e03b2
Zach Allison <zcallison@gmail.com>
2013-07-18 18:15:53 -0500

    Prompt and Alert
    Example and practical applicaiton of prompts for user input and an
    alert to display information.

---
+++ PromptAndAlert/js/script.js
@@ -0,0 +1,14 @@
+// Prompts and Alerts
+
+var width = prompt("We are calculating the area of a rectangle.\nPlease enter the width:");
+var height = prompt("Please enter the height:");
+
+var area = width * height; //Calculated with user input information
+//console.log(area); //Prints out answer to console.
+
+alert("The area of your rectangle is " + area);
+
+
+
+
+
