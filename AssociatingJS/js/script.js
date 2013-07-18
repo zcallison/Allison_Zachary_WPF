b2e61dfbd747ce2aed2c86737fd0982173d31246
Zach Allison <zcallison@gmail.com>
2013-07-18 11:58:34 -0500

    Defining Variables

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,3 +1,8 @@
-// JavaScript
+// Defining Variables
 
-var age;
+var age; //declaration
+age = 38; //definition
+
+//storage container = value
+
+var yearOfBirth = 1970; //declaring and defining on the same line
