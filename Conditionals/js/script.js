3254a8c78904aa67250d02055dc3dbf0733f406c
Zach Allison <zcallison@gmail.com>
2013-07-25 17:15:48 -0500

    Conditionals Work Along

--- Conditionals/js/script.js
+++ Conditionals/js/script.js
@@ -4,26 +4,23 @@
 // Assignment: Conditionals Screencast Work-Along
 // Date: Fri, Jul 19, 2013
 
-var kidHeight = 47;
-var minHeight = 48;
-var wParentHeight = 45;
-//var sneakerLift = 2;
 
-//if the child is old enough, print to the console "you can ride!"
-//if the child is over 48 inches in height
+//Conditional Logic - Logical Operators
 
-if(kidHeight > minHeight){
-   //you can ride!
-    console.log("You can ride the coaster!");
-} else if(kidHeight > wParentHeight ) { // you can ride with parent
-    console.log("You can ride, but only with a parent present.");
-} else { // gotta grow!
-    console.log("Sorry kid, you've got some growing to do first!");
+var budget = 100;
+var iPhonePrice = 199.99;
+//var paycheck = 200;
+var wonLottery = true;
+
+// if the price of the phone is less than the budget AND if our paycheck is over 300
+if(iPhonePrice < budget /*&& paycheck > 300*/ || wonLottery){
+    console.log("We can buy the phone!");
+
+}else{
+    console.log("No phone for you!");
 }
-
-
-/*
-if(kidHeight <= minHeight){
-    console.log("Sorry kid, you've got some growing to do first!");
-}
-*/
+/*if(paycheck > 300){
+    console.log("We can buy the phone!");
+}else{
+    console.log("No phone for you!");
+} */
