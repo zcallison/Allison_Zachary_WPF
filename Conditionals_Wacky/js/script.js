d8fb7a1bcd07ef107ca9ea27f4d3211ba996af47
Zach Allison <zcallison@gmail.com>
2013-07-26 00:02:19 -0500

    Conditionals - Wacky

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -1,5 +1,10 @@
 // Name: Zachary Allison
 // Student Number: 0003968340
 //
-// Assignment:
-// Date:
+// Assignment: Conditionals - Wacky
+// Date: 7.25.13
+
+/*
+    A calculator to determine how many servings of ramen it takes to fill a container.
+*/
+
