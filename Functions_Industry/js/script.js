1aceb825f8e946812bee45481857314295df3501
Zach Allison <zcallison@gmail.com>
2013-08-01 22:40:00 -0500

    Industry Functions

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -5,6 +5,27 @@
 // Date: 7/31/13
 
 /*
-
+ Calculator to help determine the estimated space a web server uses for a customer's site
  */
+
+// Variable declarations
+var imageCount = prompt("How many images do you have?");
+var images = new Array(imageCount);
+var i = imageCount;
+var imageNum = 1;
+var imagesPos = 0;
+
+while(i > 0){
+    images[imagesPos] = prompt("How large is image number " + imageNum + "? (In Megabytes)");
+    console.log("Image #" + imageNum + " is " + images[imagesPos] + "mb.");
+    imageNum++;
+    imagesPos++;
+    i--;
+}
+console.log(images);
+
+
+
+
+
 
